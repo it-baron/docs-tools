7d603278a0d26a2dddfb20b46a88417d2c5baa0f
Aleksei Kudriashov <akud.soft@gmail.com>
2022-12-27 19:50:55 +0200

    Suggestion discord

--- themes/cat-docs/src/widgets/deluge/MainWidget.js
+++ themes/cat-docs/src/widgets/deluge/MainWidget.js
@@ -31,12 +31,7 @@ class MainWidget extends preact.Component {
     }
 
     onInitialVote(e, state) {
-        e.stopPropagation();
-        this.setState({'state': state});
-        this.props.onSubmitVote(state);
-        if (state === false) {
-            this.props.handleOpenDrawer();
-        }
+        window.open('https://discord.gg/yn3fm8bjWU', '_blank');
     }
 
     toggleVisibility(event) {
@@ -60,65 +55,6 @@ class MainWidget extends preact.Component {
             ? 'deluge deluge-expanded'
             : 'deluge';
 
-        let body = null;
-        if (state === STATE_VOTED) {
-            body = (
-                <div>
-                    <p>Thank you for your feedback!</p>
-                    {voteAcknowledgement === 'down' && <p>If this page contains an error, you may <a
-                        class="deluge-fix-button"
-                        href="https://jira.mongodb.org/">
-                            report the problem on Jira.</a></p>}
-                    <p>We also recommend you explore <a class="deluge-fix-button"
-                        href="https://developer.mongodb.com/community/forums/">
-                            the MongoDB discussion forum</a> for additional support.</p>
-                    <p className="deluge-close-link"><small><span onClick={this.toggleVisibility}>
-                        Close</span></small></p>
-                </div>);
-        } else if (state === STATE_VOTED && !voteAcknowledgement) {
-            body = (<p>Submitting feedback...</p>);
-        } else if (typeof state === 'boolean') {
-            const sorry = (state === false)
-                ? <li>We&apos;re sorry! Please help us improve this page.</li>
-                : null;
-
-            if (canShowSuggestions) {
-                const commentBox = children[0];
-                body = (
-                    <div class="deluge-questions">
-                        <ul>
-                            {sorry}
-                            <li>{commentBox}</li>
-                        </ul>
-
-                        <div class="deluge-button-group">
-                            <button onClick={this.toggleVisibility}>Cancel</button>
-                            <button class="primary"
-                                onClick={this.onSubmitFeedback}
-                                disabled={this.props.error}>Submit</button>
-                        </div>
-                    </div>
-                );
-            } else {
-                body = (
-                    <div class="deluge-questions">
-                        <ul>
-                            {sorry}
-                            {children.map((el, i) => <li key={i}>{el}</li>)}
-                        </ul>
-
-                        <div class="deluge-button-group">
-                            <button onClick={this.toggleVisibility}>Cancel</button>
-                            <button class="primary"
-                                onClick={this.onSubmitFeedback}
-                                disabled={this.props.error}>Submit</button>
-                        </div>
-                    </div>
-                );
-            }
-        }
-
-
         return (
             <div class={delugeClass}>
                 {closed ? (
@@ -134,15 +70,13 @@ class MainWidget extends preact.Component {
                             <span class="fa fa-angle-down deluge-close-icon"
                                 onClick={this.toggleVisibility}></span>
                         </div>
-                        {state === STATE_INITIAL && (
-                            <div class="deluge-vote">
-                                <a key="voteup" id="rate-up"
-                                    onClick={(e) => this.onInitialVote(e, true)}>JOIN US ON DISCORD</a>
-                            </div>
-                        )}
+
+                        <div class="deluge-vote">
+                            <a key="voteup" id="rate-up"
+                                onClick={(e) => this.onInitialVote(e, true)}>JOIN DISCORD</a>
+                        </div>
 
                         <div class={delugeBodyClass}>
-                            {body}
                         </div>
                     </div>
                 )}
